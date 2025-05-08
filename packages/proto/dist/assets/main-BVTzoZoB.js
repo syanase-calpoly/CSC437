9b53400416cc4300323f961a36bc6fd07c40baeb
syanase-calpoly <21smyanase@gmail.com>
2025-05-07 17:08:37 -0700

    deploying

---
+++ packages/proto/dist/assets/main-BVTzoZoB.js
@@ -0,0 +1,1 @@
+import{W as r}from"./nav-bar-bO1PnE02.js";import{d as a}from"./mustang-bFTkeUv8.js";a({"warriors-nav":r});
---
+++ packages/proto/vite.config.js
@@ -0,0 +1,21 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        games: resolve(__dirname, 'games.html'),
+        players: resolve(__dirname, 'players.html'),
+        playerstats: resolve(__dirname, 'playerstats.html'),
+        standings: resolve(__dirname, 'standings.html'),
+        teamstats: resolve(__dirname, 'teamstats.html'),
+        warriors: resolve(__dirname, 'warriors.html'),
+      },
+    },
+  },
+})
