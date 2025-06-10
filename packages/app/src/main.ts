a458ce679e86d9f028a25a42b0d1c89ed77699aa
syanase-calpoly <21smyanase@gmail.com>
2025-06-09 20:39:32 -0700

    lab 14

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -2,7 +2,7 @@ import { Auth, define, History, Switch, Store } from "@calpoly/mustang";
 import { html } from "lit";
 import { Msg } from "./messages";
 import { Model, init } from "./model";
-//import update from "./update";
+import update from "./update";
 import { HeaderElement } from "./components/header.js";
 import { PlayerRoster } from "./views/players-view.js";
 import { HomeViewElement } from "./views/home-view.js";
@@ -31,13 +31,13 @@ define({
   "mu-auth": Auth.Provider,
   "mu-history": History.Provider,
   "warriors-header": HeaderElement,
-//   "mu-store": class AppStore
-//     extends Store.Provider<Model, Msg>
-//   {
-//     constructor() {
-//       super(update, init, "warriors:auth");
-//     }
-//   },
+  "mu-store": class AppStore
+    extends Store.Provider<Model, Msg>
+  {
+    constructor() {
+      super(update, init, "warriors:auth");
+    }
+  },
   "home-view": HomeViewElement,
   "player-view": PlayerRoster,
   "about-view": AboutViewElement,
